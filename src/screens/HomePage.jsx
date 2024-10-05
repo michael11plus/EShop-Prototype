693a4efa6eaf7dde5ff7a46eb69a6f6f90640267
František Majer <frantisek@brightenevo.com>
2024-10-05 14:33:57 +0200

    add react bootstrap

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
     return(
-        <>
-        </>
-        // HomePageComponenty v divu nad nami, ale nebude to prazdny fragment, udelam z toho normalni container
+        <Container fluid>
+            <Row>
+                <Col>
+                    {/* // HomePageComponenty v divu nad nami, ale nebude to prazdny fragment, udelam z toho normalni container */}
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
