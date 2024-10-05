f6c5f357e18bf06f92562298f89d23b7025e812c
František Majer <frantisek@brightenevo.com>
2024-10-05 18:11:37 +0200

    push call to be

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({
+    name,
+    type,
+    grams,
+    image
+}) => {
     const [ isVisible, setIsVisible ] = React.useState(false);
     return(
         <div className='product-card'>
-
+            <p>{name}</p>
+            <p>{type}</p>
+            <p>{grams}</p>
+            <p>{image}</p>
         </div>
     );
 };
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { ProductCard } from "../components";
 
 const HomePage = () => {
+    const [ request, setRequest ] = useState({
+        products: null,
+    })
+    const getAllProducts = async () => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        const response = await axios.get('http://localhost:5500/product', config);
+        return response;
+    }
+
+    useEffect(() => {
+        (async () => {
+            const response = await getAllProducts();
+            console.log(response.data.data);
+            const responseProduct = response.data.data;
+
+            setRequest(prev => ({
+                ...prev,
+                products: [...responseProduct.products]
+            }));
+        })();
+    }, []);
+
+    useEffect(() => {
+        console.log('this is new state: ', request);
+    }, [request]);
+    
     return(
         <Container fluid>
             <Row>
                 <Col>
-                    {/* // HomePageComponenty v divu nad nami, ale nebude to prazdny fragment, udelam z toho normalni container */}
+                    {
+                        request?.products?.map(item => {
+                            return(
+                                <ProductCard {...item} key={item.name} />
+                            );
+                        })
+                    }
                 </Col>
             </Row>
         </Container>
