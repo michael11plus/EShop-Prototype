5533135aa391a0926a8a02c51a0faa5a9daa468f
Michael A <azilinon.michael@gmail.com>
2024-12-29 18:45:52 +0100

    Update cart

--- src/components/shoppingCart/Cart.jsx
+++ src/components/shoppingCart/Cart.jsx
@@ -3,16 +3,20 @@ import { Container } from "react-bootstrap";
 import GlobalContext from '../../context/GlobalContext';
 
 const Cart = () => {
-    const { cart } = React.useContext(GlobalContext);
+    const { cart, removeFromCart, addToCart } = React.useContext(GlobalContext);
 
     return(
         <Container>
             {
                 cart.map(item => {
+                    console.log(item);
                     return(
-                        <div key={item?._id}>
+                        <div key={item?._id} className='d-flex'>
                             <img src={`../../assets/${item.image}`}/>
                             <div>{item?.name}</div>
+                            <div>{item?.count}</div>
+                            <button onClick={() => removeFromCart(item)}>-</button>
+                            <button onClick={() => addToCart(item)}>+</button>
                         </div>
                     );
                 })
